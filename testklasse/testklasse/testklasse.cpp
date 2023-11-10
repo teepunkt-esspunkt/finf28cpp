77d054b38292fccf44e2966b8c7931edaa0667fd
teepunkt-esspunkt <tsaleh@web.de>
2023-11-10 11:30:22 +0100

    freitag

---
+++ testklasse/testklasse/testklasse.cpp
@@ -0,0 +1,55 @@
+/*
+ * testklasse.cpp
+ * Programm Bechreibung
+ * Autor: Tarek Saleh
+ * Datum: 10.11.2023
+ *
+ */
+
+#include <iostream>
+#include "testklasse.h"
+using namespace std;
+
+void funktion(); //Prototyp
+// Klassenvariable, muss an dieser Stelle definiert werden
+// und existiert nur einmal für die gesamte klasse
+// (und nicht für jedes objekt einzeln)
+
+int Testklasse::anzahl = 0;
+int main()
+{
+    
+    Testklasse objekt1(42);
+    Testklasse objekt2;
+    const Testklasse konstantesObjekt(12);
+
+    //Testklasse kopiertesObjekt = objekt1 
+    Testklasse kopiertesObjekt(objekt1);
+
+    cout << "Inhalt der Variable von objekt1: " << objekt1.getVariable() << endl;
+    cout << "Inhalt der Variable von objekt2: " << objekt2.getVariable() << endl;
+    cout << "Inhalt der Variable von konstantesObjekt: " << konstantesObjekt.getVariable() << endl;
+    cout << "Inhalt der Variable von kopiertesObjekt1: " << kopiertesObjekt.getVariable() << endl;
+
+    cout << "Anzahl der Objekte: " << Testklasse::getAnzahl() << endl; // 4
+    cout << "Anzahl der Objekte: " << objekt1.getAnzahl() << endl; // geht auch: 4
+    cout << "Anzahl der Objekte: " << konstantesObjekt.getAnzahl() << endl;
+    cout << "Anzahl der Objekte: " << kopiertesObjekt.getAnzahl() << endl;
+
+    funktion();
+    cout << "Anzahl der Objekte: " << Testklasse::getAnzahl() << endl; // 4 
+
+    return 0;
+
+}
+
+void funktion()
+{
+    Testklasse lokalesObjekt(99);
+    cout << "Start der funktion()" << endl;
+    cout << "Inhalt der Variable von lokalesObjekt: " << lokalesObjekt.getVariable() << endl;
+    cout << "Anzahl der Objekte: " << Testklasse::getAnzahl() << endl;
+    cout << "Ende der funktion()" << endl;
+
+    return;
+}
---
+++ testklasse/testklasse/testklasse.h
@@ -0,0 +1,66 @@
+/*
+ * testklasse.h
+ * Programm Bechreibung
+ * Autor: Tarek Saleh
+ * Datum: 10.11.2023
+ *
+ */
+
+#pragma once
+
+class Testklasse
+{
+private:
+	// Objekt Variablen JEDES objekt dieser klasse hat eingene!
+	int variable;
+	const int konstante = 2; // Wichtig, Konstante hier vorbelegen
+
+	// klassenvariable
+	static int anzahl; // klassenvariablie gibt es nur einmal
+
+public:
+	Testklasse()		// Standard-Konstruktor (ohne Parameter)
+	{
+		variable = 1;
+		//konstante = 2;	// geht nicht, konstante ist bereits belegt
+		anzahl++;
+	}
+
+	Testklasse(int i) // Konstruktor mit Parameter
+	{
+		variable = i;
+		anzahl++;
+	}
+
+	Testklasse(Testklasse& tk)		// Kopier-Konstruktor Parameter ist eine Rferenz
+	{								// auf ein Objekt derselben Klasse (und somit automatisch
+									// ein KopierKOnstruktor
+		this->variable = tk.variable; // Variablen müssen kopiert werden
+		anzahl++;					// passiert automatisch wenn man keinen
+									// eigenen Kopier Konstruktor erstellt
+	}
+
+	//Testklasse(Testklasse& tk) = delete; // Kopier Konstruktor wird deaktiviert
+											// Objekte der KLasse werden
+										//dadurch Kopiergeschützt
+								//geht nicht wenn man einen eigenen kopierKonstruktor entwickelt hat
+
+	static int getAnzahl() // getter, static weil static, 
+	{						// static weil es sich auf eine klassen variable bezieht und nicht auf eine objekt variable
+		return anzahl;
+	}
+	int getVariable() const			// Getter (kann so auch auf const zugreifen)
+	{							// Durch angabe von const wird der getter zu einer REad Only Methode
+		return variable;		// und kann auf jedes objekt angewendet werden also auf
+								// normale und constante 
+	}
+	void setVariable(int i) // kein const! kann nicht auf konstante objekte angewendet werden (deshalb kein const)	
+	{						// weil sie daten im objekt ändert
+		variable = i;
+	}
+
+	~Testklasse()		//Destruktor (es kann nur einen geben!)
+	{
+		anzahl--;
+	}
+};		// Hier muss ein Semikolon hin
