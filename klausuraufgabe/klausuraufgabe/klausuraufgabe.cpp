6469ad617d097b540d89bfde4990c0410196cf1d
teepunkt-esspunkt <tsaleh@web.de>
2023-11-21 14:45:00 +0100

    xx

---
+++ klausuraufgabe/klausuraufgabe/klausuraufgabe.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7 };
+
+	string zahlen = "Die am haeufigsten vorkommende(n) zahl(en): ";
+	int aufkommen = 1;
+	int max = 0;
+	vector<int> dieHaeufigsten;
+
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		aufkommen = 1;
+
+		for (int j = i + 1; j < sizeof(arr) / sizeof(arr[0]); j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				aufkommen++;
+			}
+		}
+		if (aufkommen > max)
+		{
+			dieHaeufigsten.clear();
+			dieHaeufigsten.push_back(arr[i]);
+			max = aufkommen;
+
+		}
+		else if (aufkommen == max)
+		{
+			dieHaeufigsten.push_back(arr[i]);
+
+		}
+
+	}
+	for (int i = 0; i < dieHaeufigsten.size(); i++)
+	{
+		zahlen += to_string(dieHaeufigsten[i]) + ". ";
+	}
+
+
+	cout << zahlen << ": " << max << " mal" << endl;
+
+}
